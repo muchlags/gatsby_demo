5ff6362899ae3ea9e8c51951adf7552146d09a45
muchlags <muchlags@gmail.com>
2021-03-01 13:49:40 +0800

    Add files via upload

---
+++ gatsby-node.js
@@ -0,0 +1,55 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+const path = require(`path`)
+const slash = require(`slash`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(
+    `
+      {
+        allContentfulSitePost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+    .then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data", result.errors)
+      }
+
+      // Resolve the paths to our template
+      const sitePostTemplate = path.resolve("./src/pages/siteposts.js")
+
+      // Then for each result we can create a page
+      result.data.allContentfulSitePost.edges.forEach(edge => {
+        createPage({
+          path: `/siteposts/${edge.node.slug}/`,
+          component: slash(sitePostTemplate),
+          context: {
+            slug: edge.node.slug,
+            id: edge.node.id,
+          },
+        })
+      })
+    })
+    .catch(error => {
+      console.log("Error cannot retrive contentful data", error)
+    })
+}
